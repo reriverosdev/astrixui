73f32b485c490d0d5c4abe94258dbeac316a7189
Ricardo Emanuel Riveros Rivera <reriverosr@gmail.com>
2023-12-28 12:41:36 -0300

    fix: tailwind styles and project structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,26 @@
+import tailwindcss from "tailwindcss";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { dts } from "rollup-plugin-dts";
 import copy from "rollup-plugin-copy";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { Buffer } from "buffer";
 import postcss from "rollup-plugin-postcss";
-import del from "rollup-plugin-delete";
 import alias from "@rollup/plugin-alias";
+import tailwindConfig from "./tailwind.config.mjs";
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: "dist/cjs/index.js",
-        format: "cjs",
-        exports: "named",
-      },
-      {
-        file: "dist/esm/index.js",
         format: "esm",
         exports: "named",
+        dir: "dist",
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        sourcemap: true,
       },
     ],
     plugins: [
@@ -35,13 +33,9 @@ export default [
               const jsonContents = JSON.parse(contents);
 
               delete jsonContents.scripts;
-              delete jsonContents.devDependencies;
               delete jsonContents.publishConfig;
               delete jsonContents.dependencies;
-
-              jsonContents.main = "cjs/index.js";
-              jsonContents.module = "esm/index.js";
-              jsonContents.types = "index.d.ts";
+              delete jsonContents.devDependencies;
 
               return Buffer.from(JSON.stringify(jsonContents));
             },
@@ -61,6 +55,7 @@ export default [
         inject: {
           insertAt: "top",
         },
+        plugins: [tailwindcss(tailwindConfig)],
       }),
       alias({ entries: [{ find: /^@\/(.*)/, replacement: "src/$1" }] }),
       peerDepsExternal(),
@@ -69,16 +64,6 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
     ],
-    external: ["react", "react-dom", "styled-components"],
-  },
-  {
-    // path to your declaration files root
-    input: "./dist/cjs/dts/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [
-      dts(),
-      del({ hook: "buildEnd", targets: ["./dist/cjs/dts", "./dist/esm/dts"] }),
-    ],
-    external: [/\.css$/u],
+    external: ["react", "react-dom", "styled-components", "./src/stories"],
   },
 ];
